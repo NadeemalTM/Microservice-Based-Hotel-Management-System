f2319f5be55d6c75f42bfef0715ecb7947a7b71c
NadeemalTM <nadeemal@example.com>
2026-01-29 15:24:07 +0530

    backend sevice

---
+++ backend/RestaurantManagement/src/test/java/com/nsbm/group03/RestaurantManagement/RestaurantManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbm.group03.RestaurantManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/RoomManagement/src/test/java/com/nsbm/group03/RoomManagement/RoomManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbm.group03.RoomManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

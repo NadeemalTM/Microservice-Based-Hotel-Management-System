34c4e9b7ea8c902743a217d3b0711fba9f6af872
NadeemalTM <nadeemal@example.com>
2026-01-29 15:38:14 +0530

    Add eventManagementService module with initial setup, configuration, and basic structure

---
+++ backend/eventManagementService/src/test/java/com/nsbm/group03/eventManagementService/EventManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbm.group03.eventManagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

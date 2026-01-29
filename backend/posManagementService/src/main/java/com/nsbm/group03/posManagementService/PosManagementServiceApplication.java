9c0e7e1070027513f3dfe6884fa674df99f7824b
NadeemalTM <nadeemal@example.com>
2026-01-29 15:35:02 +0530

    Add posManagementService module with initial setup, configuration, and basic structure

---
+++ backend/posManagementService/src/main/java/com/nsbm/group03/posManagementService/PosManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nsbm.group03.posManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosManagementServiceApplication.class, args);
+	}
+
+}
---
+++ backend/posManagementService/src/test/java/com/nsbm/group03/posManagementService/PosManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsbm.group03.posManagementService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

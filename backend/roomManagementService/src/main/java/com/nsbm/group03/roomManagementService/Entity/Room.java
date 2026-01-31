e44bb5de5a89432797c81e1c9adfa6104746c1d6
RashmikaDilshan <dilshanrdv@gmail.com>
2026-01-31 07:41:11 +0530

    feat: add Room entity with JPA mappings

---
+++ backend/roomManagementService/src/main/java/com/nsbm/group03/roomManagementService/Entity/Room.java
@@ -0,0 +1,75 @@
+package com.nsbm.group03.roomManagementService.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Room {
+
+    @Id
+    private String roomId;
+    private String roomNumber;
+    private String roomType;
+    private double pricePerNight;
+    private int capacity;
+    private String status;
+
+    
+    public Room() {
+    }
+
+    public Room(String roomId, String roomNumber, String roomType, double pricePerNight, int capacity, String status) {
+        this.roomId = roomId;
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.pricePerNight = pricePerNight;
+        this.capacity = capacity;
+        this.status = status;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+    public String getRoomType() {
+        return roomType;
+    }
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+    public void setPricePerNight(double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+    public int getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Room [roomId=" + roomId + ", roomNumber=" + roomNumber + ", roomType=" + roomType + ", pricePerNight="
+                + pricePerNight + ", capacity=" + capacity + ", status=" + status + "]";
+    }   
+
+
+
+}
